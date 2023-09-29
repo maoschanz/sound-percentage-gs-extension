83c7576a2bbc08702ac0de17f613e7c746968cff
Ignaz Kraft <ignaz.k@live.de>
2023-09-30 01:13:18 +0200

    Update to Gnome 45

--- sound-percentage@maestroschan.fr/extension.js
+++ sound-percentage@maestroschan.fr/extension.js
@@ -1,109 +1,99 @@
 // sound-percentage@maestroschan.fr/extension.js
 // GPL v3
 // Copyright Romain F. T. 2018-2022
+// Copyright Ignaz Kraft  2023-2023
 
-const { Clutter, St } = imports.gi;
-const Main = imports.ui.main;
-const Volume = imports.ui.status.volume;
+import Clutter from 'gi://Clutter';
+import St from 'gi://St';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const ShellVersion = imports.misc.config.PACKAGE_VERSION;
-let PRIMARY_SIGNAL_ID, INPUT_SIGNAL_ID, INPUT_VISIBLE_SIGNAL_ID;
+export default class SoundPercentageExtension {
+	OUTPUT_SIGNAL_ID = undefined;
+	INPUT_SIGNAL_ID = undefined;
+	INPUT_STREAM_ADDED_SIGNAL_ID = undefined;
+	INPUT_STREAM_REMOVED_SIGNAL_ID = undefined;
+	//---------------------------------------------------------------------------
+	getVolumeInput() {
+		return Main.panel.statusArea.quickSettings._volumeInput;
+	}
+	getVolumeOutput() {
+		return Main.panel.statusArea.quickSettings._volumeOutput;
+	}
+	updateVolume() {
+		for (const indicator of [this.getVolumeOutput(), this.getVolumeInput()]) {
+			let percent = '';
+			let virtualMax = 0;
+			let muted = false;
+			let error = false;
+			const IO = indicator._output || indicator._input;
 
-function init() {}
+			try {
+				muted = IO._stream.is_muted;
+			} catch (e) {
+				error = true;
+			}
 
-//------------------------------------------------------------------------------
+			try {
+				virtualMax = indicator._control.get_vol_max_norm();
+			} catch (e) {
+				error = true;
+			}
 
-function showLabel(outputPercentage, inputPercentage) {
-	let volumeIndicator = Main.panel.statusArea.quickSettings._volume;
+			if (error) {
+				percent = '?';
+			} else {
+				let volume = muted ? 0 : IO.stream.volume;
+				percent = Math.round(volume / virtualMax * 100) + '%';
+			}
 
-	volumeIndicator._outputPercentageLabel.text = outputPercentage;
-	volumeIndicator._inputPercentageLabel.text = inputPercentage;
+			if (!indicator._indicator.visible) {
+				percent = '';
+			}
+
+			indicator._percentageLabel.text = percent;
+		}
+	}
+	//---------------------------------------------------------------------------
+	connect() {
+		let output = this.getVolumeOutput()._output;
+		let input = this.getVolumeInput()._input;
+
+		const self = this;
+		const update = () => {self.updateVolume()};
+
+		this.OUTPUT_SIGNAL_ID = output.connect('stream-updated', update);
+		this.INPUT_SIGNAL_ID = input.connect('stream-updated', update);
+		this.INPUT_STREAM_ADDED_SIGNAL_ID = input._control.connect('stream-added', update);
+		this.INPUT_STREAM_REMOVED_SIGNAL_ID = input._control.connect('stream-removed', update);
+	}
+	disconnect() {
+		let output = this.getVolumeOutput()._output;
+		let input = this.getVolumeInput()._input;
+
+		output.disconnect(this.OUTPUT_SIGNAL_ID);
+		input.disconnect(this.INPUT_SIGNAL_ID);
+		input._control.disconnect(this.INPUT_STREAM_ADDED_SIGNAL_ID);
+		input._control.disconnect(this.INPUT_STREAM_REMOVED_SIGNAL_ID);
+	}
+	//---------------------------------------------------------------------------
+	enable() {
+		for (const indicator of [this.getVolumeOutput(), this.getVolumeInput()]) {
+			indicator._percentageLabel = new St.Label({
+				y_expand: true,
+				y_align: Clutter.ActorAlign.CENTER
+			});
+
+			indicator.add(indicator._percentageLabel);
+			indicator.set_child_at_index(indicator._percentageLabel, 1);
+			indicator.add_style_class_name('power-status');
+		}
+
+		this.updateVolume();
+		this.connect();
+	}
+	disable() {
+		this.disconnect();
+		this.getVolumeOutput()._percentageLabel.destroy();
+		this.getVolumeInput()._percentageLabel.destroy();
+	}
 }
-
-function updateVolume() {
-	let volumeIndicator = Main.panel.statusArea.quickSettings._volume;
-
-	let outputPercent = 0, inputPercent = 0;
-	let virtMax, outputMuted, outputVolume, inputMuted, inputVolume;
-
-	try {
-		outputMuted = volumeIndicator._output._stream.is_muted;
-	} catch (e) {
-		outputMuted = true;
-		outputPercent = '?';
-	}
-
-	try {
-		inputMuted = volumeIndicator._input._stream.is_muted;
-	} catch (e) {
-		inputMuted = true;
-		inputPercent = '?';
-	}
-
-	try {
-		virtMax = volumeIndicator._control.get_vol_max_norm();
-	} catch (e) {
-		outputMuted = true;
-		inputMuted = true;
-		outputPercent = '?';
-		inputPercent = '?';
-	}
-
-	if (!outputMuted) {
-		let volume = volumeIndicator._output.stream.volume;
-		outputPercent = Math.round(volume / virtMax * 100);
-	}
-
-	if (!inputMuted) {
-		let volume = volumeIndicator._input.stream.volume;
-		inputPercent = Math.round(volume / virtMax * 100);
-	}
-
-	outputPercent += '%';
-	inputPercent += '%';
-	if (!volumeIndicator._inputIndicator.visible) {
-		inputPercent = '';  // no idea how to handle this better
-	}
-
-	showLabel(outputPercent, inputPercent);
-}
-
-//------------------------------------------------------------------------------
-
-function enable() {
-	let volumeIndicator = Main.panel.statusArea.quickSettings._volume;
-
-	volumeIndicator._outputPercentageLabel = new St.Label({
-		y_expand: true,
-		y_align: Clutter.ActorAlign.CENTER
-	});
-	volumeIndicator._inputPercentageLabel = new St.Label({
-		y_expand: true,
-		y_align: Clutter.ActorAlign.CENTER
-	});
-
-	volumeIndicator.add(volumeIndicator._outputPercentageLabel);
-	volumeIndicator.set_child_at_index(volumeIndicator._outputPercentageLabel, 1);
-	volumeIndicator.add(volumeIndicator._inputPercentageLabel);
-	volumeIndicator.add_style_class_name('power-status');
-
-	updateVolume();
-	PRIMARY_SIGNAL_ID = volumeIndicator._output.connect('stream-updated', updateVolume);
-	INPUT_SIGNAL_ID = volumeIndicator._input.connect('stream-updated', updateVolume);
-	INPUT_STREAM_ADDED_SIGNAL_ID = volumeIndicator._input._control.connect('stream-added', updateVolume);
-	INPUT_STREAM_REMOVED_SIGNAL_ID = volumeIndicator._input._control.connect('stream-removed', updateVolume);
-}
-
-function disable() {
-	let volumeIndicator = Main.panel.statusArea.quickSettings._volume;
-
-	volumeIndicator._output.disconnect(PRIMARY_SIGNAL_ID);
-	volumeIndicator._input.disconnect(INPUT_SIGNAL_ID);
-	volumeIndicator._input._control.disconnect(INPUT_STREAM_ADDED_SIGNAL_ID);
-	volumeIndicator._input._control.disconnect(INPUT_STREAM_REMOVED_SIGNAL_ID);
-	volumeIndicator._outputPercentageLabel.destroy();
-	volumeIndicator._inputPercentageLabel.destroy();
-}
-
-//------------------------------------------------------------------------------
-
